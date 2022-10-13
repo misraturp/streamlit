2f136c63ea76c19fe04dce5df3d67a7e800bf321
Kamil Bregula <kamil.bregula@snowflake.com>
2022-10-13 03:16:16 +0200

    fixup! Fix pydoc command for streamlit package

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -19,6 +19,7 @@ import os
 import re
 import subprocess
 import sys
+import tempfile
 import textwrap
 import unittest
 from unittest.mock import patch
@@ -160,11 +161,12 @@ class StreamlitTest(unittest.TestCase):
     def test_aaa(self):
         cwd = os.getcwd()
         try:
+            os.chdir(tempfile.mkdtemp())
             # Run the script as a separate process to make sure that
             # the currently loaded modules do not affect the test result.
             output = subprocess.check_output(
                 [sys.executable, "-m", "pydoc", "streamlit"]
-            )
+            ).decode()
             self.assertIn("Help on package streamlit:", output)
         finally:
             os.chdir(cwd)
